f77f3905505207b6189edca045252405a516db63
kunljha <jha711kunal@gmail.com>
2021-11-27 23:06:19 +0530

    basic navbar & home component setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import './App.css'
+import Home from './components/Home'
+import Navbar from './components/Navbar'
 
 const App = () => {
-	return <div className='App'>Hello from React App</div>
+	return (
+		<div className='App'>
+			<Navbar />
+			<div className='container'>
+				<Home />
+			</div>
+		</div>
+	)
 }
 
 export default App
---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+const Navbar = () => {
+	return (
+		<nav>
+			<ul>
+				<li>
+					<a href='/'>Home</a>
+				</li>
+				<li>
+					<a href='/create'>Create Blog</a>
+				</li>
+			</ul>
+		</nav>
+	)
+}
+
+export default Navbar
